19670436d716846688ad0da519360b453b7f741d
DanielLemeIESB <167783257+DanielLemeIESB@users.noreply.github.com>
2024-06-20 01:00:53 -0300

    Update2 utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
+import os
+import json
+import gdown
 from pathlib import Path
-
 import streamlit as st
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
@@ -9,16 +11,31 @@ from langchain_community.vectorstores.faiss import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_openai.chat_models import ChatOpenAI
-
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 
 from configs import *
 
-_ = load_dotenv(find_dotenv())
+# URL do arquivo JSON no Google Drive
+gdrive_url = 'https://drive.google.com/uc?id=1oTfj9BCKxPtowyRHADsdHagQBfGsiisb'
 
+# Caminho temporário para salvar o arquivo JSON
+temp_json_path = '/tmp/api_key.json'
+
+# Baixar o arquivo JSON do Google Drive
+gdown.download(gdrive_url, temp_json_path, quiet=False)
+
+# Ler a chave da API do arquivo JSON
+with open(temp_json_path, 'r') as f:
+    data = json.load(f)
+    api_key = data['OPENAI_API_KEY']
+
+# Definir a variável de ambiente com a chave da API
+os.environ['OPENAI_API_KEY'] = api_key
+
+# Carregar as variáveis de ambiente do arquivo .env
+load_dotenv()
 
 PASTA_ARQUIVOS = Path(__file__).parent / 'arquivos'
-
 
 def importacao_documentos():
     documentos = []
@@ -76,3 +93,11 @@ def cria_chain_conversa():
     )
 
     st.session_state['chain'] = chat_chain
+
+def main():
+    with st.sidebar:
+        sidebar()
+    chat_window()
+
+if __name__ == '__main__':
+    main()
