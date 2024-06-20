105d2ec54cb6efb3374cb6c1df314775f500176a
DanielLemeIESB <167783257+DanielLemeIESB@users.noreply.github.com>
2024-06-20 01:59:54 -0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-MODEL_NAME = 'gpt-3.5-turbo-0125'
+MODEL_NAME = 'GPT-4o'
 RETRIEVAL_SEARCH_TYPE = 'mmr'
 RETRIEVAL_KWARGS = {"k": 5, "fetch_k": 20}
 PROMPT = '''Você é um Chatbot amigável que auxilia na interpretação 
